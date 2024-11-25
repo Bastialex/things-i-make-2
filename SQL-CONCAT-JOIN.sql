bd7e423faf8bf746ececbe4ba6aa4dac7579663f
Bastián Cortés <bbastian.1995@gmail.com>
2024-11-25 12:45:17 -0300

    Update SQL-CONCAT-JOIN (Ejemplo).sql

--- SQL-CONCAT-JOIN.sql
+++ SQL-CONCAT-JOIN.sql
@@ -1,15 +1,15 @@
 SELECT 
-Concat(`run.a`,'-',`dv.a`) as RunAlumno,
-Concat(`nombre.a`,' ',`apellido.a`) as NombreAlumno,
-Concat(`nombre.p`,' ',`apellido.p`) as NombreProfesor,
-`nombre.s` as Sala,
-`nombre.c` as Clase
+Concat(a.run,'-',a.dv) as RunAlumno,
+Concat(a.nombre,' ',a.apellido) as NombreAlumno,
+Concat(p.nombre,' ',p.apellido) as NombreProfesor,
+s.nombre as Sala,
+c.nombre as Clase
 FROM 
 clase_alumno as ca
 INNER JOIN alumno AS a ON a.id = ca.id_alumno
 INNER JOIN profesor AS p ON p.id = ca.id_profesor
 INNER JOIN sala AS s ON s.id = ca.id_sala
 INNER JOIN clase AS c ON c.id = ca.id_clase
-WHERE `edad.a` > 20
-ORDER BY Concat(`apellido.a`,' ',`nombre.a`) ASC;
+WHERE a.edad > 20
+ORDER BY Concat(a.apellido,' ',a.nombre) ASC;
 
